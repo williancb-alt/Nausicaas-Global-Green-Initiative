49ee67d955fa3db6ae051c1f1574a99f89d4aa99
John Dennehy <johndennehy101@gmail.com>
2026-02-03 16:19:58 +0000

    #66: Update terraform storage account name

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "nausicaas-tfstate-rg"
-    storage_account_name = "nausicaastfstate94916"
+    storage_account_name = "nausicaastfstate"
     container_name       = "tfstate"
     key                  = "infra/terraform.tfstate"
   }
