fa70c29b9abb1693c9ecd559bbd25febb49c2596
John Dennehy <johndennehy101@gmail.com>
2026-02-04 15:31:13 +0000

    #66: Update resource names

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "nausicaas-tfstate-rg"
-    storage_account_name = "nausicaastfstate"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "nausicaastate"
     container_name       = "tfstate"
     key                  = "infra/terraform.tfstate"
   }
