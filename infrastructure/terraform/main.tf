0fbbb8f05be84b14237c7c3ac1dc39ee0619dea3
Anujkr-devOps <L00188535@atu.ie>
2026-02-07 16:59:24 +0000

    Fix 2 for RG creation.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,9 +1,9 @@
-data "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "main" {
   name = var.resource_group_name
   location = var.location
 }
 
-data "azurerm_container_registry" "acr" {
+resource"azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
