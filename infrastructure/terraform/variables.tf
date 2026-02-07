229001bca4ed8f9e52b40a96928880a9f1b39f77
Anujkr-devOps <L00188535@atu.ie>
2026-02-07 17:28:04 +0000

    removed duplicate values from variable.tf

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -14,63 +14,6 @@ variable "acr_name" {
   type = string
 }
 
-# -----------------------------
-# AKS Cluster
-# -----------------------------
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  dns_prefix          = var.aks_dns_prefix
-
-  # Simple + cost friendly defaults (good for student / assignment)
-  sku_tier = "Free"
-
-  default_node_pool {
-    name       = "system"
-    node_count = var.aks_node_count
-    vm_size    = var.aks_vm_size
-
-    # Use Azure CNI overlay if you want later; for now keep default (kubenet)
-    # vnet_subnet_id = ...
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin = "kubenet"
-    load_balancer_sku = "standard"
-  }
-
-  # Optional: enable RBAC (recommended)
-  role_based_access_control_enabled = true
-}
-
-# -----------------------------
-# Allow AKS to pull images from ACR
-# -----------------------------
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
-
-# Outputs (handy for kubectl later)
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
-output "aks_rg" {
-  value = azurerm_kubernetes_cluster.aks.resource_group_name
-}
-
-output "aks_kube_config_raw" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
-
 variable "aks_name" {
   type    = string
   default = "nausicaas-aks"
