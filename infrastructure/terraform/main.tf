a884b5eae308abe9922b07cf203142979639b823
Anujkr-devOps <L00188535@atu.ie>
2026-02-07 16:25:30 +0000

    updated RG names and pointed in main - updated worfklow wih RG name.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,12 +1,16 @@
-resource "azurerm_resource_group" "main" {
-  name     = "nausicaas-rg"
-  location = var.location
+data "azurerm_resource_group" "main" {
+  name = var.resource_group_name
 }
 
-resource "azurerm_container_registry" "acr" {
+data "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku                 = "Basic"
-  admin_enabled       = true
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.main.name
+}
+
+output "acr_login_server" {
+  value = data.azurerm_container_registry.acr.login_server
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -2,9 +2,14 @@
 
 variable "location" {
   type    = string
-  default = "germanywestcentral"
+  default = "northeurope"
 }
 
 variable "acr_name" {
   type = string
+}
+
+Variable "resource_group_name" {
+  type    = string
+  default = "nausicaas-rg"
 }
