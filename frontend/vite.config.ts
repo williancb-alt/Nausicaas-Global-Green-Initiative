a69877dadcfff3792b6ed34bd2da586cde725431
John Dennehy <johndennehy101@gmail.com>
2026-02-16 21:42:03 +0000

    #78: Initial attempt at codecov implementation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+    },
   },
 })
