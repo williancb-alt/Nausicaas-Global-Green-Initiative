8d84924405f8eb70af271348867f7c561517accc
L00203060 <L00203060@atu.ie>
2026-03-23 18:36:30 +0000

    Remove unused imports from merge migration to fix lint errors

--- backend/migrations/versions/5fff0faad733_merge_heads.py
+++ backend/migrations/versions/5fff0faad733_merge_heads.py
@@ -5,9 +5,6 @@ Revises: f80615169851, f9dea3dcfe50
 Create Date: 2026-03-23 18:31:55.449615
 
 """
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = '5fff0faad733'
