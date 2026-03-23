305bb47e70e561b6cb588c42f601a54c9f0b3c96
L00203060 <L00203060@atu.ie>
2026-03-23 18:32:19 +0000

    Merge migration heads after main merge

---
+++ backend/migrations/versions/5fff0faad733_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge_heads
+
+Revision ID: 5fff0faad733
+Revises: f80615169851, f9dea3dcfe50
+Create Date: 2026-03-23 18:31:55.449615
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5fff0faad733'
+down_revision = ('f80615169851', 'f9dea3dcfe50')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
